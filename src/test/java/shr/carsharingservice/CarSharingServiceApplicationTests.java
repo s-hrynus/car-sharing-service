8740495ed51449c260bc239e2a7f1e421c9a9383
Stanislav Hrynus <stanislav.hrynus@gmail.com>
2023-12-08 23:05:37 +0200

    Initial commit

---
+++ src/test/java/shr/carsharingservice/CarSharingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package shr.carsharingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
